ec6af02ef9eb1c857073813e97bee937977ef9a2
Katie Brey <kebrey@ncsu.edu>
2017-02-19 16:59:36 -0500

    Adding transaction views and relationships

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,7 @@
 class Transaction < ApplicationRecord
   belongs_to :to_account, :class_name => 'Account'
   belongs_to :from_account, :class_name => 'Account'
+  def self.all_pending
+    self.where(:approval_status => 'pending')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,12 @@ Rails.application.routes.draw do
   resources :accounts
   resources :friend
   resources :transaction
+
+  #Admin navigation
+  get 'admin/home', to: 'user#admin_home'
+  get 'admin/manage_admins', to: 'user#manage_admins'
+  #accounts - direct /accounts path
+  get 'admin/manage_users', to: 'user#manage_users'
+  get 'admin/manage_transactions', to: 'transaction#manage'
+
 end
