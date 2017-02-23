24bbf88ce7c00ea5a8a92a2684403e05a34d772a
Katie Brey <kebrey@ncsu.edu>
2017-02-23 01:23:13 -0500

    adding edit profile

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create(id: '0', name: 'Admin', email: 'admin@ncsu.edu', password: 'password', is_admin: 1)
 User.create(id: '1', name: 'User', email: 'user@ncsu.edu', password: 'password', is_admin: 0)
+User.create(id: '2', name: 'Another', email: 'user2@ncsu.edu', password: 'password', is_admin: 0)
+
+Account.create(id:1, account_number: '121212121', user_id: 1, status:'active', balance:1000)
+Account.create(id:2, account_number: '343434343', user_id: 2, status:'active', balance:2000)
+
+Transaction.create(id:1, txn_type:'send', amount:50, to_account:'121212121', from_account:'343434343', status:'pending')
