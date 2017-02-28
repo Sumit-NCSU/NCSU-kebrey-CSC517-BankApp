60bf3d7c2961fa1d13a5731cdf58fd25316dfce8
Sumit Srivastava <srivassumit@gmail.com>
2017-02-28 03:04:49 -0500

    adding test cases

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: 'TestUser', email: 'testuser@gmail.com',
+                     password: 'password')
+  end
+
+  test 'user should be valid' do
+    assert @user.valid?
+  end
+
+  test 'user name should not be blank' do
+    @user.name = '    '
+    assert_not @user.valid?
+  end
+
+  test 'user email should not be blank' do
+    @user.email = '   '
+    assert_not @user.valid?
+  end
+
+  test 'user email addresses should not be duplicate' do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test 'password should be present' do
+    @user.password = @user.password_confirmation = ' '
+    assert_not @user.valid?
+  end
+
+  test 'password should not be too short' do
+    @user.password = @user.password_confirmation = 'a' * 5
+    assert_not @user.valid?
+  end
+
+  test 'password should not be too long' do
+    @user.password = @user.password_confirmation = 'a' * 57
+    assert_not @user.valid?
+  end
+
 end
