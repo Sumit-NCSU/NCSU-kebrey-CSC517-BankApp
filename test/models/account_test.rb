e9b228d141d539d7a44d002c6859b7f00a1b0ea6
Sumit Srivastava <ssrivas8@ncsu.edu>
2017-03-03 03:07:15 -0500

    added test cases

--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class AccountTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: 'TestUser', email: 'testuser@gmail.com',
+                     password: 'password')
+    @account = Account.new(status: 'active', balance: 9.99, user: @user)
+  end
+
+  test 'account should be valid' do
+    assert @account.valid?
+  end
+
+  test 'account status should be valid' do
+    @account.status = 'random'
+    assert_not @account.valid?
+  end
+
 end
--- test/models/transaction_test.rb
+++ test/models/transaction_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class TransactionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @transaction = Transaction.new(txn_type: 'deposit', amount: 9.99, start_date: '2017-02-19 03:15:03',
+        effective_date: '2017-02-19 03:15:03', status: 'pending')
+  end
+
+  test 'transaction should be valid' do
+    assert @transaction.valid?
+  end
+
+  test 'transaction type should be valid' do
+    @transaction.txn_type = 'random'
+    assert_not @transaction.valid?
+  end
+
+  test 'transaction status should be valid' do
+    @transaction.status = 'random'
+    assert_not @transaction.valid?
+  end
+
 end
